ea8bfdec24d5435cc3982cb86d8ef3e5a3a37e57
Diego Coura <diego@quatrodois.com.br>
2025-02-25 19:13:30 -0300

    fixed compatibility issue with 4+ django versions

--- admin_sso/forms.py
+++ admin_sso/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 from admin_sso import settings
 from admin_sso.models import Assignment
